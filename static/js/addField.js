700e8a23c709f228a3df7cd86964614796ae0174
ekongg <eric.kong98@gmail.com>
2015-02-28 15:55:04 -0500

    Fixed Jquery that Adds User Fields

---
+++ static/js/addField.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var maxFields = 8;
+	var wrapper = $(".input_fields");
+	var addButton = $(".add_field");
+
+	var i = 1;
+	$(addButton).click(function(e){
+		e.preventDefault();
+		if(i < maxFields){
+			i++;
+			$(wrapper).append('<div><input type="text" name="users[]"/> <a href="#" class="remove_field">Remove</a></div>');
+		}
+	});
+
+	$(wrapper).on("click", ".remove_field", function(e){
+		e.preventDefault();
+		$(this).parent('div').remove();
+		x--;
+	});
+});

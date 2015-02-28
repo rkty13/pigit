d34393c84988553d664d655f9368e25b00bf0336
ekongg <eric.kong98@gmail.com>
2015-02-28 16:34:02 -0500

    bugs

--- static/js/addField.js
+++ static/js/addField.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
 		e.preventDefault();
 		if(i < maxFields){
 			i++;
-			$(wrapper).append('<div><input type="text" name="users'+i+'"/> <a href="#" class="remove_field">Remove</a></div>');
+			$(wrapper).append('<div><input type="text" name="user'+i+'"/> <a href="#" class="remove_field">Remove</a></div>');
 		}
 	});
 
 	$(wrapper).on("click", ".remove_field", function(e){
 		e.preventDefault();
 		$(this).parent('div').remove();
-		x--;
+		i--;
 	});
 });

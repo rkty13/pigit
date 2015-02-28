1e723417f3f18756377bf656a32e9e71c7ff6c47
Robert <rkty13@gmail.com>
2015-02-28 15:34:54 -0500

    more process updates

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template, request
 import os
 import subprocess
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
+def checkRepoExists(repo_name):
+	(stdout, stderr) = Popen(["ls", "repos"], stdout=PIPE).communicate();
+	repo_list = stdout.split("\n")
+	if repo_name in repo_list:
+		return True
+	else:
+		return False
+
 def createRepo(repo_name, user_list):
-	subprocess.call(["touch", "repos/test.txt"])
+	if (not checkRepoExists(repo_name)):
+		repo_dir = "repos/" + str(repo_name)
+		subprocess.call(["groupadd", repo_name])
+		for user in user_list:
+			subprocess.call(["groups", user, "-aG", repo_name])
+		subprocess.call(["mkdir", repo_dir])
+		subprocess.call(["cd", repo_dir])
+		subprocess.call(["git", "init", "--bare"])
+		subprocess.call(["touch", "repos/test.txt"])
+		return True
+	else:
+		return False
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,8 +33,10 @@ def index():
 		repo_name = request.form['repo_name']
 		user_list = []
 		user_list.append(request.form['user1'])
-		createRepo(repo_name, user_list)
-		return render_template("index.html")
+		if (createRepo(repo_name, user_list)):
+			return render_template("index.html")
+		else:
+			return render_template("index.html", error="Repo Exists")
 
 	return render_template("index.html")
 

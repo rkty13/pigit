e708348179f47511cdcee91f421bf6dc5b58222c
Robert <rkty13@gmail.com>
2015-02-28 13:47:59 -0500

    bash

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 import os
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	(stdout, stderr) = Popen(["ls", "../"], stdout=PIPE).communicate()
+	return render_template("index.html", stdout=stdout)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
